9d59285e8603cb676a9ca48b7bc2d54f1f500b65
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2024-10-11 22:28:52 +0600

    Merged start and pause button into one
    It will make UX more efficient

--- js/app.js
+++ js/app.js
@@ -1,45 +1,52 @@
-let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-let timerRef = document.querySelector('.display-time');
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timerRef = document.querySelector(".display-time");
 let int = null;
+let isRunning = false;
 
-document.getElementById('start-timer').addEventListener('click', ()=>{
-    if(int!==null){
-        clearInterval(int);
-    }
-    int = setInterval(displayTimer,10);
-});
+var startPauseTimer = document.getElementById("start-pause-timer");
 
-document.getElementById('pause-timer').addEventListener('click', ()=>{
+startPauseTimer.addEventListener("click", () => {
+  if (isRunning) {
     clearInterval(int);
+    isRunning = false;
+    startPauseTimer.innerHTML = "Start";
+  } else {
+    int = setInterval(displayTimer, 10);
+    isRunning = true;
+    startPauseTimer.innerHTML = "Pause";
+  }
 });
 
-document.getElementById('reset-timer').addEventListener('click', ()=>{
-    clearInterval(int);
-    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+document.getElementById("reset-timer").addEventListener("click", () => {
+  clearInterval(int);
+  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+  timerRef.innerHTML = "00 : 00 : 00 : 000 ";
 });
 
-function displayTimer(){
-    milliseconds+=10;
-    if(milliseconds == 1000){
-        milliseconds = 0;
-        seconds++;
-        if(seconds == 60){
-            seconds = 0;
-            minutes++;
-            if(minutes == 60){
-                minutes = 0;
-                hours++;
-            }
-        }
+function displayTimer() {
+  milliseconds += 10;
+  if (milliseconds == 1000) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes == 60) {
+        minutes = 0;
+        hours++;
+      }
     }
+  }
 
-    let h = hours < 10 ? "0" + hours : hours;
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    let s = seconds < 10 ? "0" + seconds : seconds;
-    let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  let ms =
+    milliseconds < 10
+      ? "00" + milliseconds
+      : milliseconds < 100
+      ? "0" + milliseconds
+      : milliseconds;
 
-    timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
-
-
+  timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
 }
